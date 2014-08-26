02e8c7d37736c287955802b48fc89d6d0f3ccf3e
Colin Mattson <colinmattson@gmail.com>
2014-08-26 12:59:38 -0700

    Initial commit

---
+++ lib/orm_adapter/nobrainer.rb
@@ -0,0 +1,7 @@
+require "orm_adapter/nobrainer/version"
+
+module OrmAdapter
+  module Nobrainer
+    # Your code goes here...
+  end
+end
---
+++ lib/orm_adapter/nobrainer/version.rb
@@ -0,0 +1,5 @@
+module OrmAdapter
+  module Nobrainer
+    VERSION = "0.0.1"
+  end
+end
